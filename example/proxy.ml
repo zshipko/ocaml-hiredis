8434bcf89fcb8f59a685a0a707676b6b7c1727ee
zach shipko <zachshipko@gmail.com>
2017-03-27 13:40:27 -0700

    add example

---
+++ example/proxy.ml
@@ -0,0 +1,20 @@
+open Hiredis
+open Lwt.Infix
+
+let pool = Pool.create ~port:6379 "127.0.0.1" 64
+
+let handler args =
+    Pool.use pool (fun cli ->
+        Lwt.return_some (Client.run_v cli args))
+
+let on_exn exc =
+    print_endline (Printexc.to_string exc)
+
+let main =
+Server.create (`TCP (`Port 1234)) >>= fun srv ->
+Server.run ~on_exn srv handler
+
+let () = Lwt_main.run main
+
+
+
